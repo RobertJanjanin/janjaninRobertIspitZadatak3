58571c161e256f9ec02e3243ac17cfe540761d8e
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-13 11:21:13 +0100

    Add files via upload

---
+++ janjaninRobertZadatak3/janjaninRobertZadatak3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace janjaninRobertZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unos;
+            Console.WriteLine("upisi rijec: ");
+            unos=Console.ReadLine();
+            for (int i = unos.Length - 1; i > 0; i--)
+            {
+
+                Console.WriteLine(unos.Substring(i,1));
+
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
